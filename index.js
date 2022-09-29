5fa011e34b05dcd0a4a5625efd04ed5f30971b50
Syed Ahamed <SYEDAHAMED29>
2022-09-30 04:25:08 +0530

    Added media queries and loaders

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ let advice = document.querySelector(".advice");
 
 const baseUrl = "https://api.adviceslip.com/advice";
 
+function showLoader() {
+   adviceId.innerText = "";
+   advice.innerText = "";
+   adviceId.classList.add("is-loading");
+   advice.classList.add("is-loading");
+}
+
+function hideLoader() {
+   adviceId.classList.remove("is-loading");
+   advice.classList.remove("is-loading");
+}
+
 function getData() {
+   showLoader();
    fetch(baseUrl)
       .then((response) => response.json())
       .then((data) => {
          console.log(data.slip.advice);
          adviceId.innerText = `ADVICE #${data.slip.id}`;
          advice.innerText = data.slip.advice;
+         hideLoader();
       })
       .catch((err) => console.log(err));
 }
